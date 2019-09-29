a2f3208dc6af363d1f4153172738db27bf5a8c7d
RustamAV <rustam.alashrafov@gmail.com>
2019-09-29 16:08:00 +0200

    Added user authentication using JWT

--- server/server.js
+++ server/server.js
@@ -22,6 +22,8 @@ mongoose
 
 // Use Routes
 app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
