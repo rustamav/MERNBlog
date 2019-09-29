71cd562b2f8e205727631ed043eb763ddd330234
RustamAV <rustam.alashrafov@gmail.com>
2019-09-29 18:00:46 +0200

    Added auth function.

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const config = require('config');
 const jwt = require('jsonwebtoken');
-//const auth = require('../../middleware/auth');
+const auth = require('../../middleware/auth');
 
 
 // User Model
@@ -50,13 +50,13 @@ router.post('/', (req, res) => {
     })
 });
 
-// // @route   GET api/auth/user
-// // @desc    Get user data
-// // @access  Private
-// router.get('/user', auth, (req, res) => {
-//   User.findById(req.user.id)
-//     .select('-password')
-//     .then(user => res.json(user));
-// });
+// @route   GET api/auth/user
+// @desc    Get user data
+// @access  Private
+router.get('/user', auth, (req, res) => {
+  User.findById(req.user.id)
+    .select('-password')
+    .then(user => res.json(user));
+});
 
 module.exports = router;
--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const auth = require("../../middleware/auth");
 
 // Post Model
 const Item = require('../../models/Post');
@@ -16,7 +17,7 @@ router.get('/', (req, res) => {
 // @route   POST api/posts
 // @desc    Create a post
 // @access  Public
-router.post('/', (req, res) => {
+router.post('/', auth, (req, res) => {
   const newPost = new Post({
     title: req.body.title,
     body: req.body.body,
@@ -29,7 +30,7 @@ router.post('/', (req, res) => {
 // @route   DELETE api/posts/:id
 // @desc    Delete A post
 // @access  Private
-router.delete('/:id', (req, res) => {
+router.delete('/:id', auth, (req, res) => {
   Item.findById(req.params.id)
     .then(item => item.remove().then(() => res.json({ success: true })))
     .catch(err => res.status(404).json({ success: false }));
