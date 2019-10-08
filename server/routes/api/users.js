739d389a93f2d74b914662ae5670f9fccf842c51
RustamAV <rustam.alashrafov@gmail.com>
2019-10-08 15:05:55 +0200

    Swithced to async await

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -23,47 +23,41 @@ router.post('/', [
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() })
     }
-
-    const { name, email, password } = req.body;
     try {
+      const { name, email, password } = req.body;
       // Check for existing user
-      User.findOne({ email })
-        .then(user => {
-          if (user) return res.status(400).json({ msg: 'User already exists' });
+      let user = await User.findOne({ email });
+      
+      if (user) return res.status(400).json({ msg: 'User already exists' });
+      
+      user = new User({
+        name,
+        email,
+        password
+      });
 
-          const newUser = new User({
-            name,
-            email,
-            password
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      jwt.sign(
+        { id: user.id },
+        config.get('jwtSecret'),
+        { expiresIn: 3600 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({
+            token,
+            user: {
+              id: user.id,
+              name: user.name,
+              email: user.email
+            }
           });
-
-          // Create salt & hash
-          bcrypt.genSalt(10, (err, salt) => {
-            bcrypt.hash(newUser.password, salt, (err, hash) => {
-              if (err) throw err;
-              newUser.password = hash;
-              newUser.save()
-                .then(user => {
-                  jwt.sign(
-                    { id: user.id },
-                    config.get('jwtSecret'),
-                    { expiresIn: 3600 },
-                    (err, token) => {
-                      if (err) throw err;
-                      res.json({
-                        token,
-                        user: {
-                          id: user.id,
-                          name: user.name,
-                          email: user.email
-                        }
-                      });
-                    }
-                  )
-                });
-            })
-          })
-        });
+        }
+      );
     }
     catch (error) {
       console.error(error.message);
