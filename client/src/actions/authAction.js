4ccd112827c9c360f13a5ffdf7e3c6a5b2efcbb4
RustamAV <rustam.alashrafov@gmail.com>
2019-10-08 17:48:34 +0200

    Added jwt tockets authorisation with Auth reducer.

---
+++ client/src/actions/authAction.js
@@ -0,0 +1,36 @@
+import {
+    REGISTER_SUCCESS,
+    REGISTER_FAIL
+} from './types';
+import {setAlert} from './alertAction';
+import axios from 'axios';
+
+export const register = ({name, email, password}) => async dispatch => {
+    const config = {
+        headers: {
+            'Content-type': 'application/json'
+        }
+    }
+
+    const body = JSON.stringify({name, email, password});
+
+    try {
+        const res = await axios.post('/api/users', body, config);
+        dispatch({
+            type: REGISTER_SUCCESS,
+            payload: res.data
+        });
+    } 
+    catch(error){
+        console.log(error);
+        const errors = errors.response.data.errors;
+
+        if(errors){
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+        }
+
+        dispatch({
+            type:REGISTER_FAIL
+        });
+    }
+}
